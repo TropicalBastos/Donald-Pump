9f45f6e45c95c79d76938631ad801c6414daacf5
Ian Bastos <ian-bastos@live.com>
2018-01-13 23:28:40 +0000

    first phase of how to play scene

--- scenes/howtoplay.lua
+++ scenes/howtoplay.lua
@@ -1,5 +1,3 @@
-package.path = package.path .. ";../?.lua"
-
 local composer = require( "composer" )
 
 local scene = composer.newScene()
@@ -15,29 +13,6 @@ local screenTop = display.screenOriginY
 local screenLeft = display.screenOriginX
 local bottomMarg = display.contentHeight - display.screenOriginY
 local rightMarg = display.contentWidth - display.screenOriginX
-
-
-local bg
-local title = "How to Play"
-local info = "The Goal of the game is simple, hit as many balloons as possible avoiding the nukes and get an epic highscore!"
-local subInfo = "The following balloon rules apply:"
-local pumpInfo = "Normal Donald: 1 point for each property token"
-local ultraInfo = "Donald Pump: x10 multiplier, score = property tokens x 10"
-local toupeInfo = "The Toupe: cascade, get points for each balloon it hits"
-local propertyInfo = "Property Token: +1 property token, lose all tokens and its game over!"
-local bombInfo = "DPRK Nuke: Touch it and its game over, so tap carefully"
-local titleObj
-local infoObj
-local subInfoObj
-local balloonContainer
-local pumpObj
-local ultraObj
-local toupeObj
-local propertyObj
-local nukeObj
-local objStartCol = 500
-local objArray
-local textForObjArray
 local backBtn
 
 -- -----------------------------------------------------------------------------------
@@ -51,144 +26,31 @@ function scene:create( event )
     -- Code here runs when the scene is first created but has not yet appeared on screen
 
     composer.removeScene("scenes.menu")
-    bg = display.newImage("res/howtoplay.png", centerX, centerY)
-    bg.width = rightMarg + 100
-    bg.height = bottomMarg + 100
-    --balloonContainer = display.new
+    local bg = display.newImage("res/howtoplay.png")
+    bg.width = rightMarg + 50
+    bg.height = bottomMarg + 50
+    bg.x = centerX
+    bg.y = centerY
 
-    appodeal.hide( "banner" )
-
-    pumpObj = display.newImage("res/pumpballoon.png")
-    ultraObj = display.newImage("res/ultraballoon.png")
-    toupeObj = display.newImage("res/toupeballoon.png")
-    propertyObj = display.newImage("res/propertyballoon.png")
-    nukeObj = display.newImage("res/bombballoon.png")
     backBtn = display.newSprite(backSheet, backSeq)
     backBtn:play()
-
-    pumpText = display.newText({text=pumpInfo, font=ruleFont, align="center", width=100, height=0})
-    ultraText = display.newText({text=ultraInfo, font=ruleFont, align="center", width=100, height=0})
-    toupeText = display.newText({text=toupeInfo, font=ruleFont, align="center", width=100, height=0})
-    propertyText = display.newText({text=propertyInfo, font=ruleFont, align="center", width=100, height=0})
-    nukeText = display.newText({text=bombInfo, font=ruleFont, align="center", width=100, height=0})
-
-    textForObjArray = {pumpText, ultraText, toupeText, propertyText, nukeText}
-    objArray = {pumpObj, ultraObj, toupeObj, propertyObj, nukeObj}
-    for i=1, #objArray do
-        objArray[i].width = 65
-        objArray[i].height = 80
-        --local xPos = (i * objArray[i].width) + 20
-        local xPos
-        if i == 1 then
-            xPos = centerX - (objArray[i].width + 40)
-        elseif i == 2 then
-            xPos = centerX
-        elseif i == 3 then
-            xPos = centerX + (objArray[i].width + 40)
-        elseif i == 4 then
-            xPos = centerX - (objArray[i].width + 40)
-        elseif i == 5 then
-            xPos = centerX + (objArray[i].width + 40)
-        end 
-        objArray[i].x = xPos
-        -- textForObjArray[i].width = 65
-        -- textForObjArray[i].height = 40 
-        textForObjArray[i].x = xPos
-    end
-    nukeObj.width = 35
 
     --back button dimensions
     backBtn:scale(0.6, 0.6)
     backBtn.y = bottomMarg - 35
     backBtn.x = 1000
 
-    local titleOptions = {
-        text = title,
-        font = titleFont,
-        x = display.contentCenterX,
-        y = 1000
-    }
-    local infoOptions = {
-        text = info,
-        font = howFont,
-        x = display.contentCenterX,
-        y = 1000,
-        width = rightMarg/1.4
-    }
-    local subInfoOptions = {
-        text = subInfo,
-        font = howFont,
-        x = display.contentCenterX,
-        y = 1000,
-        width = rightMarg/1.4
-    }
-
-    titleObj = display.newText(titleOptions)
-    infoObj = display.newText(infoOptions)
-    subInfoObj = display.newText(subInfoOptions)
-
-    local tr = {
-        time = 500,
-        y = 15,
-        transition = easing.outCubic
-    }
-    local tr2 = {
-        time = 700,
-        y = 60,
-        transition = easing.outCubic
-    }
-    local tr3 = {
-        time = 900,
-        y = 110,
-        transition = easing.outCubic
-    }
-    local balloonTr = {
-        time = 1100,
-        y = 170,
-        transition = easing.outCubic
-    }
-    local descTr = {
-        time = 1100,
-        y = 240,
-        transition = easing.outCubic
-    }
     local backTr = {
         time = 1200,
         x = centerX + 15,
         transition = easing.inCubic
     }
 
-    transition.to(titleObj, tr)
-    transition.to(infoObj, tr2)
-    transition.to(subInfoObj, tr3)
     transition.to(backBtn, backTr)
-    for i=1, #objArray do
-        balloonTr.time = balloonTr.time + 200
-        if i >= 4 then
-            transition.to(objArray[i], {
-                time = 1100,
-                y = 315,
-                transition = easing.outCubic
-            })
-        else
-            transition.to(objArray[i], balloonTr)
-        end
-    end
-    for i=1, #textForObjArray do
-        descTr.time = descTr.time + 200
-        if i >= 4 then
-            transition.to(textForObjArray[i], {
-                time = 1100,
-                y = 390,
-                transition = easing.outCubic
-            })
-        else 
-            transition.to(textForObjArray[i], descTr)
-        end
-    end
 
+    sceneGroup:insert(bg)
+    sceneGroup:insert(backBtn)
 end
-
 
 --back event listener
 function goBackToMain()
@@ -200,6 +62,7 @@ function goBackToMain()
         onComplete = function() composer.gotoScene("scenes.menu", {effect="crossFade", time=500}) end
     })
 end
+
 
 -- show()
 function scene:show( event )
@@ -238,14 +101,7 @@ function scene:destroy( event )
 
     local sceneGroup = self.view
     -- Code here runs prior to the removal of scene's view
-    for i=1, #objArray do 
-        objArray[i]:removeSelf()
-        textForObjArray[i]:removeSelf()
-    end
-    titleObj:removeSelf()
-    infoObj:removeSelf()
-    subInfoObj:removeSelf()
-    bg:removeSelf()
+
 end
 
 
